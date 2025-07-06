5918abc589c95907e3028423d5d38c03ec8123a6
ziqiangao <88281267+ziqiangao@users.noreply.github.com>
2025-07-06 19:57:56 +0100

    merge

--- src/level.c
+++ src/level.c
@@ -2,8 +2,8 @@
 #include <gb/cgb.h>
 #include <stdint.h>
 #include "menuscreen.h"
-#include "tiles.c"
-#include "tileset.c"
+#include "tiles.h"
+#include "tileset.h"
 #include "gb-madness.h"
 #include "register.h"
 #include "metatiles.h"
@@ -12,35 +12,71 @@
 #include "icon1.h"
 #include "physics.h"
 
-static uint8_t x = 80;
 static uint8_t frame = 0;
 
-void set_metatile_xy(uint8_t bx, uint8_t by, uint8_t midx) {
-    const unsigned char *mt = metatiles[midx];
-    set_bkg_tiles(bx, by, 1, 1, &mt[0]);
-    set_bkg_tiles(bx + 1, by, 1, 1, &mt[1]);
-    set_bkg_tiles(bx, by + 1, 1, 1, &mt[2]);
-    set_bkg_tiles(bx + 1, by + 1, 1, 1, &mt[3]);
+uint16_t current_loaded_map_x = 0;
+uint16_t current_loaded_map_y = 0;
+
+const unsigned int background_palette[] = {
+    0x7FFF,
+    0x5294,
+    0x2A52,
+    0x0000
+};
+
+void load_map_segment(uint16_t map_start_x_tiles, uint16_t map_start_y_tiles) {
+    current_loaded_map_x = map_start_x_tiles;
+    current_loaded_map_y = map_start_y_tiles;
+
+    for (uint8_t by_vram = 0; by_vram < BKG_MAP_HEIGHT_TILES; by_vram++) {
+        for (uint8_t bx_vram = 0; bx_vram < BKG_MAP_WIDTH_TILES; bx_vram++) {
+            uint16_t full_map_tile_x = map_start_x_tiles + bx_vram;
+            uint16_t full_map_tile_y = map_start_y_tiles + by_vram;
+
+            uint8_t tile_idx_to_load = 0;
+
+            if (full_map_tile_x < MAP_TILE_WIDTH && full_map_tile_y < MAP_TILE_HEIGHT) {
+                uint8_t metatile_x = full_map_tile_x / 2;
+                uint8_t metatile_y = full_map_tile_y / 2;
+                uint8_t metatile_idx = level_map[metatile_y * LEVEL_MAP_WIDTH + metatile_x];
+
+                if (metatile_idx < NUM_METATILES) {
+                    const unsigned char *mt = metatiles[metatile_idx];
+                    if ((full_map_tile_y % 2) == 0) {
+                        if ((full_map_tile_x % 2) == 0) {
+                            tile_idx_to_load = mt[0];
+                        } else {
+                            tile_idx_to_load = mt[1];
+                        }
+                    } else {
+                        if ((full_map_tile_x % 2) == 0) {
+                            tile_idx_to_load = mt[2];
+                        } else {
+                            tile_idx_to_load = mt[3];
+                        }
+                    }
+                }
+            }
+            set_bkg_tiles(bx_vram, by_vram, 1, 1, &tile_idx_to_load);
+        }
+    }
 }
 
 static void setup(void) {
     stopall();
     SPRITES_8x16;
     SHOW_SPRITES;
+
     set_bkg_data(0, 64, tiles_tiles);
-    uint16_t idx = 0;
-    for (uint8_t by = 0; by < LEVEL_MAP_HEIGHT; by++) {
-        for (uint8_t bx = 0; bx < LEVEL_MAP_WIDTH; bx++) {
-            uint8_t m = level_map[idx++];
-            if (m < NUM_METATILES) {
-                set_metatile_xy(bx * 2, by * 2, m);
-            }
-        }
-    }
+
+    set_bkg_palette(0, 1, background_palette);
+
+    load_map_segment(0, 0); 
     SHOW_BKG;
+
     set_sprite_palette(0, icon1_PALETTE_COUNT, icon1_palettes);
     set_sprite_data(icon1_TILE_ORIGIN, icon1_TILE_COUNT, icon1_tiles);
-    move_metasprite(icon1_metasprites[frame], icon1_TILE_ORIGIN, 0, x, (cube_y / SCALE) - 8);
+
     play(1);
 }
 
@@ -49,6 +85,7 @@ void dolevel(void) {
 
     while (1) {
         uint8_t joy = joypad();
+
         if (joy & J_START) {
             HIDE_SPRITES;
             domenu();
@@ -56,11 +93,19 @@ void dolevel(void) {
         }
 
         handle_jump(joy);
+        handle_horizontal_movement(joy);
+
         update_physics();
+        
         hide_metasprite(icon1_metasprites[frame], 0);
-        frame++;
-        if (frame >= 24) frame = 0;
-        move_metasprite(icon1_metasprites[frame], icon1_TILE_ORIGIN, 0, x, (cube_y / SCALE) - 8);
+
+        frame = (frame + 1);
+        if (frame >= 25) frame = 0; 
+
+        move_metasprite(icon1_metasprites[frame], icon1_TILE_ORIGIN, 0,
+                        cube_x_pixel + 10,
+                        (cube_y / SCALE) + 16);
+        
         delay(16);
         wait_vbl_done();
     }
--- src/physics.c
+++ src/physics.c
@@ -1,18 +1,67 @@
 #include <gb/gb.h>
 #include "physics.h"
+#include "icon1.h"     // Para los pivots del sprite
+#include "gb-madness.h" // Para LEVEL_MAP_WIDTH, LEVEL_MAP_HEIGHT, level_map
+#include "collision.h" // Para la l�gica de colisi�n (is_pixel_in_solid_metatile)
 
-int16_t cube_y = GROUND_Y * SCALE;
+// Variables de estado del cubo
+int16_t cube_x_pixel = 80;
+// Posici�n inicial Y: Asentar el sprite sobre el GROUND_Y (128).
+// El tope del sprite estar� en (GROUND_Y - icon1_PIVOT_Y - icon1_PIVOT_H)
+int16_t cube_y = (GROUND_Y - icon1_PIVOT_Y - icon1_PIVOT_H) * SCALE; // (128 - 1 - 14) * 100 = 11300
+
 int16_t velocity_y = 0;
 uint8_t is_jumping = 0;
+
 
 void update_physics(void) {
     velocity_y += GRAVITY_PER_FRAME;
     cube_y += velocity_y;
 
-    if (cube_y >= GROUND_Y * SCALE) {
-        cube_y = GROUND_Y * SCALE;
-        velocity_y = 0;
-        is_jumping = 0;
+    int16_t current_sprite_y_pixel = cube_y / SCALE;
+
+    int16_t check_x_left = cube_x_pixel + icon1_PIVOT_X;
+    int16_t check_x_right = cube_x_pixel + icon1_PIVOT_X + icon1_PIVOT_W - 1;
+
+    // --- Colisi�n Vertical con el Suelo (ca�da) ---
+    if (velocity_y > 0) { // Solo si est� cayendo
+        int16_t bottom_y_collision_point = current_sprite_y_pixel + icon1_PIVOT_Y + icon1_PIVOT_H - 1;
+
+        if (is_pixel_in_solid_metatile(check_x_left, bottom_y_collision_point) ||
+            is_pixel_in_solid_metatile(check_x_right, bottom_y_collision_point)) {
+
+            // Calcular la posici�n Y del tope del metatile s�lido impactado
+            int16_t metatile_top_y = (bottom_y_collision_point / 16) * 16;
+
+            // Ajustar la posici�n Y del cubo para que se siente exactamente sobre el metatile
+            // Su punto de colisi�n inferior debe estar 1 p�xel por encima del metatile_top_y
+            // (cube_y / SCALE) + icon1_PIVOT_Y + icon1_PIVOT_H - 1 = metatile_top_y - 1
+            // (cube_y / SCALE) = metatile_top_y - icon1_PIVOT_Y - icon1_PIVOT_H
+            cube_y = (metatile_top_y - icon1_PIVOT_Y - icon1_PIVOT_H) * SCALE;
+            
+            velocity_y = 0;     // Detener la ca�da
+            is_jumping = 0;     // No est� saltando (ha aterrizado)
+        }
+    }
+    // --- Colisi�n Vertical con el Techo (salto hacia arriba) ---
+    else if (velocity_y < 0) { // Solo si est� subiendo
+        int16_t top_y_collision_point = current_sprite_y_pixel + icon1_PIVOT_Y;
+
+        if (is_pixel_in_solid_metatile(check_x_left, top_y_collision_point) ||
+            is_pixel_in_solid_metatile(check_x_right, top_y_collision_point)) {
+
+            // Calcular la posici�n Y del pie del metatile s�lido impactado
+            // El pie del metatile es (metatile_y * 16) + 15
+            // Si el top_y_collision_point est� en el metatile (mt_y), su fondo es (mt_y+1)*16 -1
+            int16_t metatile_bottom_y = ((top_y_collision_point / 16) + 1) * 16 - 1;
+
+            // Ajustar la posici�n Y del cubo para que est� justo debajo del metatile
+            // (cube_y / SCALE) + icon1_PIVOT_Y = metatile_bottom_y + 1
+            // (cube_y / SCALE) = metatile_bottom_y + 1 - icon1_PIVOT_Y
+            cube_y = (metatile_bottom_y + 1 - icon1_PIVOT_Y) * SCALE;
+            
+            velocity_y = 0; // Detener el movimiento hacia arriba
+        }
     }
 }
 
@@ -21,4 +70,48 @@ void handle_jump(uint8_t joy) {
         velocity_y = JUMP_VELOCITY;
         is_jumping = 1;
     }
+}
+
+void handle_horizontal_movement(uint8_t joy) {
+    int16_t new_x_pixel = cube_x_pixel;
+    
+    if (joy & J_LEFT) {
+        new_x_pixel -= HORIZONTAL_SPEED;
+    }
+    if (joy & J_RIGHT) {
+        new_x_pixel += HORIZONTAL_SPEED;
+    }
+
+    if (new_x_pixel != cube_x_pixel) {
+        int16_t top_y_pivot = (cube_y / SCALE) + icon1_PIVOT_Y;
+        int16_t bottom_y_pivot = (cube_y / SCALE) + icon1_PIVOT_Y + icon1_PIVOT_H - 1;
+
+        if (new_x_pixel < cube_x_pixel) { // Movi�ndose a la izquierda
+            int16_t check_x = new_x_pixel + icon1_PIVOT_X;
+            // Si choca con un metatile s�lido por la izquierda
+            if (is_pixel_in_solid_metatile(check_x, top_y_pivot) ||
+                is_pixel_in_solid_metatile(check_x, bottom_y_pivot)) {
+                
+                // Ajustar al borde derecho del metatile colisionado
+                // metatile_x_right_edge = ((check_x / 16) + 1) * 16 - 1
+                // new_x_pixel + icon1_PIVOT_X = metatile_x_right_edge + 1
+                // new_x_pixel = metatile_x_right_edge + 1 - icon1_PIVOT_X
+                new_x_pixel = (((check_x / 16) + 1) * 16 - icon1_PIVOT_X);
+            }
+        } else if (new_x_pixel > cube_x_pixel) { // Movi�ndose a la derecha
+            int16_t check_x = new_x_pixel + icon1_PIVOT_X + icon1_PIVOT_W - 1;
+            // Si choca con un metatile s�lido por la derecha
+            if (is_pixel_in_solid_metatile(check_x, top_y_pivot) ||
+                is_pixel_in_solid_metatile(check_x, bottom_y_pivot)) {
+                
+                // Ajustar al borde izquierdo del metatile colisionado
+                // metatile_x_left_edge = (check_x / 16) * 16
+                // new_x_pixel + icon1_PIVOT_X + icon1_PIVOT_W - 1 = metatile_x_left_edge - 1
+                // new_x_pixel = metatile_x_left_edge - 1 - icon1_PIVOT_X - icon1_PIVOT_W + 1
+                // new_x_pixel = metatile_x_left_edge - icon1_PIVOT_X - icon1_PIVOT_W
+                new_x_pixel = ((check_x / 16) * 16 - icon1_PIVOT_X - icon1_PIVOT_W);
+            }
+        }
+    }
+    cube_x_pixel = new_x_pixel;
 }
