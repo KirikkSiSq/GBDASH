03b51408f5e45524e72dfe5ae01db869562491dc
ziqiangao <88281267+ziqiangao@users.noreply.github.com>
2025-07-15 14:24:05 +0100

    develop kit support for online player

--- htmlplayer/lsdjhandler.js
+++ htmlplayer/lsdjhandler.js
@@ -6,6 +6,7 @@ let groove
 //Pattern Counters
 let chainp1, chainp2, chainwa, chainno
 let phasep1, phasep2, phasewa, phaseno
+let currnote
 
 //elements
 const container = document.getElementById("LSDPlayer")
@@ -18,23 +19,118 @@ const notes = [
     'C ', 'C#', 'D ', 'D#', 'E ', 'F ', 'F#', 'G ', 'G#', 'A ', 'A#', 'B '
 ]
 
+const noisenotes = []
+
+for (let i = 0x00; i <= 0x3B; i++) {
+  noisenotes.push(i.toString(16).toUpperCase().padStart(2, '0') + ' ');
+}
+
+const noiseShortModeNotes = [
+  // -9
+  'D-9', 'F-9', 'G-9',
+  // -8 to -1
+  'C-8', 'D-8', 'F-8', 'G-8',
+  'C-7', 'D-7', 'F-7', 'G-7',
+  'C-6', 'D-6', 'F-6', 'G-6',
+  'C-5', 'D-5', 'F-5', 'G-5',
+  'C-4', 'D-4', 'F-4', 'G-4',
+  'C-3', 'D-3', 'F-3', 'G-3',
+  'C-2', 'D-2', 'F-2', 'G-2',
+  'C-1', 'D-1', 'F-1', 'G-1',
+  // 0 to 4
+  'C 0', 'D 0', 'F 0', 'G#0',
+  'C 1', 'D 1', 'F 1', 'G#1',
+  'C 2', 'D 2', 'F 2', 'G#2',
+  'C 3', 'D 3', 'F 3', 'G#3',
+  'C 4', 'D 4', 'F 4', 'G#4',
+  // 5
+  'C 5', 'F 5',
+  // 6 to 8
+  'C 6', 'C 7', 'C 8'
+];
+
+let words = [
+    'W-0',
+    'W-1',
+    'W-3',
+    'W-4',
+    'W-5',
+    'W-6',
+    'W-7',
+    'W-8',
+    'W-9',
+    'W-A',
+    'W-B',
+    'W-C',
+    'W-D',
+]
+
+const allNoiseNotes = noisenotes.concat(noiseShortModeNotes);
+
 function getpulsenote(note) {
-    note = note - 1
-    if (note == -1 || typeof (note) == 'undefined') { return '---' }
+    note--
+    if (note == -1 || typeof(note) == 'undefined') { return '---' }
+    return `${notes[note % 12]}${(Math.floor(note / 12) + 2).toString(16).toUpperCase()}`
+}
 
-    if (note > 107)
-
-        return `${notes[note % 12]}${Math.floor(note / 12) + 2}`
+function resetWordTable() {
+    words = [
+        'W-0',
+        'W-1',
+        'W-3',
+        'W-4',
+        'W-5',
+        'W-6',
+        'W-7',
+        'W-8',
+        'W-9',
+        'W-A',
+        'W-B',
+        'W-C',
+        'W-D',
+    ];
 }
 
 function getwavenote(note, instrument) {
-    type = module.Instruments[instrument].Type
+    const instrumentData = module.Instruments[instrument];
+    
+
+    // Wave channel using word table
     if (instrument == 0x40) {
-        return;
+        note--;
+        if (note < 0) return '---';
+        if (note >= words.length) return '---';
+        return words[note];
     }
-    if (type == 'KIT') {
-        return;
+
+    const type = instrumentData.Type;
+
+    // KIT instrument type
+    if (type === 'KIT') {
+        const kit1Index = (note >> 4) - 1;
+        const kit2Index = (note & 0b1111) - 1;
+
+        const kit1 = kits[instrumentData.Kit[0]];
+        const kit2 = kits[instrumentData.Kit[1]];
+
+        const sample1 = (kit1Index < 0) ? '---' : (kit1Index >= kit1.sample_names.length ? 'OFF' : kit1.sample_names[kit1Index]);
+        const sample2 = (kit2Index < 0) ? '---' : (kit2Index >= kit2.sample_names.length ? 'OFF' : kit2.sample_names[kit2Index]);
+
+        return [sample1, sample2];
     }
+
+    // Default wave note
+    note--;
+    if (note < 0) return '---';
+    return `${notes[note % 12]}${(Math.floor(note / 12) + 1).toString(16).toUpperCase()}`;
+}
+
+
+function getnoisenote(note) {
+    if (note >= allNoiseNotes.length || note < 0) {
+        return '???'
+    }
+    return allNoiseNotes[note]
 }
 
 function updatespeedinfo() {
@@ -43,10 +139,6 @@ function updatespeedinfo() {
     } else {
         document.getElementById("LSDSpeed").innerText = `Speed ${speeds.join(':')} @ ${2 * tempo / 5}hz/${tempo}bpm, Groove ${groove}`
     }
-}
-
-function displaychains(p1s, p2s, was, nos, scroll) {
-
 }
 
 function setupLSDJ() {
